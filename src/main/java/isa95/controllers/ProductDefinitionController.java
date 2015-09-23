4d05085c301f76ad7de3ed3a002115f588b2eb0b
Alberto Coppini <skioppetto@gmail.com>
2015-09-23 23:57:58 +0200

    prepared crud controller for product-definition

---
+++ src/main/java/isa95/controllers/ProductDefinitionController.java
@@ -0,0 +1,33 @@
+package isa95.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by user on 23/09/15.
+ */
+
+@RestController
+@RequestMapping("/product-definitions")
+public class ProductDefinitionController {
+
+    @RequestMapping(method = RequestMethod.POST)
+    String create(){
+        return "Hello World!";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    String retrieve(@PathVariable("id") String productDefinitionID){
+        return "retrieve ".concat(productDefinitionID);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    String update(@PathVariable("id") String productDefinitionID){
+        return "updating object";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    String delete(@PathVariable("id") String productDefinitionID){
+        return "deleting ".concat(productDefinitionID);
+    }
+
+}
