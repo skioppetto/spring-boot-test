b74134a00daa6e19ae2e63bab8286381b76514c2
Alberto Coppini <skioppetto@gmail.com>
2015-09-23 12:01:22 +0200

    Initial commit

---
+++ src/main/java/me/test/Application.java
@@ -0,0 +1,17 @@
+package me.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by user on 23/09/15.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
